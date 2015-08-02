6fdf6e375ab9f517f71e2916268a3079d19027a4
trandatnh <trandatnh@yahoo.com>
2015-08-02 15:27:35 +0800

    for team B

--- app/src/main/java/com/daynhauhoc/courtcouter/MainActivity.java
+++ app/src/main/java/com/daynhauhoc/courtcouter/MainActivity.java
@@ -9,12 +9,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int scoreTeamA;
+    private int scoreTeamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scoreTeamA = 0;
+        scoreTeamB = 0;
         displayForTeamA(scoreTeamA);
     }
 
@@ -61,5 +63,29 @@ public class MainActivity extends AppCompatActivity {
     public void addTeamAOnePoints(View view) {
         scoreTeamA += 1;
         displayForTeamA(scoreTeamA);
+    }
+
+
+    /**
+     * Displays the given scoreTeamB for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.score_team_b);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addTeamBThreePoints(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTeamBTwoPoints(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTeamBOnePoints(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
     }
 }
